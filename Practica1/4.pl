49ef8bc9f099856f1c2cebcac927dce18e4cc271
pepeargentoo <dani_rc_22@hotmail.com>
2024-04-08 23:08:45 -0300

    ejercicio 4

---
+++ Practica1/4.pl
@@ -0,0 +1,6 @@
+4. Dados los siguientes predicados: hombre(unHombre). mujer(unaMujer). padres(persona, madre, padre).
+a. Construya una base de hechos con los miembros de su familia. b. Defina las siguientes reglas:
+• hermana/2, donde hermana(A,B) significa que A es hermana de B.
+• nieto/2, donde nieto(A,B) significa que A es el nieto de B.
+• abuelo/2, donde abuelo(A,B) significa que A es el abuelo de B.
+• tia/2, donde tia(A,B) significa que A es la tía de B. Esta regla definirla, en una primera instancia, valiéndose sólo de los hechos disponibles. En una segunda instancia, valiéndose de alguna otra regla que pudieron haber definido previamente.
